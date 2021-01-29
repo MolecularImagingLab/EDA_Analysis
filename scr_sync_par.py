07bf0f3e9d7c9a17452705de1325a5f717321a8c
Rami Hamati <rhamati95@gmail.com>
2021-01-28 21:45:01 -0500

    Manual Sync Condensed Script

--- scr_sync_par.py
+++ scr_sync_par.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-
 #! /home/lauri/anaconda3/bin/python
+
 #load libraries
 import os, sys 
 import pandas as pd
 import numpy as np
-from nilearn import plotting
-import matplotlib.pyplot as plt
 import math
 
 def readin():
@@ -18,119 +16,74 @@ def readin():
     return input_scr, input_par, output_par
 
 def scr_mod(input_scr):
-#Reads in scr file and adds headers
+    #Reads in scr file and adds headers
+    input_scr, input_par, output_par = readin()
     scr = pd.read_table(input_scr, header=0, names=['microsiemens', 'ch1', 'ch2', 'ch3'], delim_whitespace=True)
-# Add time as a column, at a sampling rate of 200 Hz
+    # Add time as a column, at a sampling rate of 200 Hz
     scr = scr.assign(time=[0 + (0.005)*i for i in range(len(scr))])[['time'] + scr.columns.tolist()]
-# Start at iloc of 0 defines the time @ first electrical impulse - start1
-    start1 = (scr.loc[scr['ch2']==5, 'time'].iloc[0])
-# Every 10-11th float aftwerwards denotes an electrical impulse - starts,  except where there are test shocks
-    starts = dict(scr.loc[scr['ch2']==5, 'time'])
-    allstarts = []
-    for l in list(starts.values()):
-        allstarts.append(l)
-    npallstarts = np.array(allstarts)
-    allstartsrounded = (npallstarts.round(decimals=0)).tolist()
+    # View data
+    # scr.plot.line(x='time', y=['microsiemens','ch1','ch2','ch3'])
+    # Start at iloc of 0 defines the time @ first electrical impulse - start1
+    # start1 = (scr.loc[scr['ch2']==5, 'time'].iloc[0])
+    # Every 10-11th float aftwerwards denotes an electrical impulse - starts,  except where there are test shocks
+    starts = list(scr.loc[scr['ch2']==5, 'time'])
+    allstartsrounded = [round(num, 0) for num in starts]
+    # Remove duplicates
     allstartsunique = []
     for r in allstartsrounded:
         if r not in allstartsunique:
             allstartsunique.append(r)
-            x = allstartsrounded.count(r)
-            if x > 11:
-                allstartsunique.remove(r)
     print('the following numbers are possible start times for the experiment:', allstartsunique)
-# Startstop at iloc of 0 defines the time @ last electrical impulse of first run - end1
-# Every 10-11th float afterwards denotes the starts and stops of the runs, except where there are test shocks
+    # Startstop at iloc of 0 defines the time of the last electrical impulse of first run - end1
+    # end1 = (scr.loc[scr['ch1']==5, 'time'].iloc[0])
+    # Every 10-11th float afterwards denotes the starts and stops of the runs, except where there are test shocks
+    startstops = list(scr.loc[scr['ch1']==5, 'time'])
+    # Removes values that are smaller than start1
     allstartstops = []
-    allstartstop = dict(scr.loc[scr['ch1']==5, 'time'])
-# Removes values that are smaller than start1
-    for l in list(allstartstop.values()):
-        if l > start1:
+    for l in startstops:
+         if l > start1:
             allstartstops.append(l)
-    npstartstop = np.array(allstartstops)
-    startstoprounded = (npstartstop.round(decimals=0)).tolist()
-# Test shocks register more than 11 times with proximate values
+    allstartstoprounded = [round(num, 0) for num in allstartstops]
+    # Remove duplicates
     startstopunique = []
-    for r in startstoprounded:
+    for r in allstartstoprounded:
         if r not in startstopunique:
             startstopunique.append(r)
-            x = startstoprounded.count(r)
-            if x > 11:
-                startstopunique.remove(r)
     print('the following numbers are possible stop and start times for the experiment:', startstopunique)
-    startstopunique = []
-# If 4 starts and 4 stops are accounted for, ensure they are within time limit for a single run
+    startstopunique_list = []
+    # If 4 starts and 4 stops are accounted for, ensure they are within time limit for a single run
     print('enter rounded starts and stops - a total of 8 sequential whole numbers:')
     for i in range(0, 8): # set up loop to run 8 times
         number = int(input('Please enter a start or stop:')) # prompt user for number
-        startstopunique.append(number)
-    print(startstopunique)
-#Check if input values are valid
+        startstopunique_list.append(number)
+    print(startstopunique_list)
+    # Check if input values are valid
     count=0
-    finalstartstop = []
-    if startstopunique[1] - startstopunique[0] == 483:
-        count =+ 1
-        finalstartstop.append(startstopunique[0])
-        finalstartstop.append(startstopunique[1])
-    if startstopunique[3] - startstopunique[2] == 483:
-        count =+ 2
-        finalstartstop.append(startstopunique[2])
-        finalstartstop.append(startstopunique[3])
-    if startstopunique[5] - startstopunique[4] == 483:
-        count =+ 3 
-        finalstartstop.append(startstopunique[4])
-        finalstartstop.append(startstopunique[5])
-    if startstopunique[7] - startstopunique[6] == 483:
-        count =+ 4
-        finalstartstop.append(startstopunique[6])
-        finalstartstop.append(startstopunique[7])
-# Confirms all 4 runs are accounted for
+    finalstartstops = []
+    for number in [0, 2, 4, 6]:
+        if startstopunique_list[number+1] - startstopunique_list[number] == 483:
+            count = count +1 
+            finalstartstops.append(startstopunique_list[number])
+            finalstartstops.append(startstopunique_list[number+1])
+    # Confirms all 4 runs are accounted for
     if count ==4:
         print('number of runs accounted for:', count)
-# If all four runs of a session are accounted for, starts and stops are set below by finding exact value
-    finalnumbers = []
-    start1 = []
-    end1 = []
-    start2 = []
-    end2 = []
-    start3 = []
-    end3 = []
-    start4 = []
-    end4 = []
-    for l in allstarts:
-        if math.isclose(finalstartstop[0], l, abs_tol = 1):
-            finalnumbers.append(l)
-            start1.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[2], l, abs_tol = 1):
-            finalnumbers.append(l)
-            start2.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[4], l, abs_tol = 1):
-            finalnumbers.append(l)
-            start3.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[6], l, abs_tol = 1):
-            finalnumbers.append(l)
-            start4.append(finalnumbers[0])
-            finalnumbers = []
-    for l in allstartstops:
-        if math.isclose(finalstartstop[1], l, abs_tol = 1):
-            finalnumbers.append(l)
-            end1.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[3], l, abs_tol = 1):
-            finalnumbers.append(l)
-            end2.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[5], l, abs_tol = 1):
-            finalnumbers.append(l)
-            end3.append(finalnumbers[0])
-            finalnumbers = []
-        if math.isclose(finalstartstop[7], l, abs_tol = 1):
-            finalnumbers.append(l)
-            end4.append(finalnumbers[0])
-            finalnumbers = []
+    # If all four runs of a session are accounted for, starts and stops are set below by finding exact value
+    finalnumbers, start1, end1, start2, end2, start3, end3, start4, end4 = ([] for i in range(0,9))
+    starts_list = [start1, start2, start3, start4]
+    for z,i in enumerate([0,2,4,6]): # count, item
+        for l in starts:
+            if math.isclose(finalstartstops[i], l, abs_tol = 1):
+                finalnumbers.append(l)
+                starts_list[z].append(finalnumbers[0])
+                finalnumbers = []
+    ends_list = [end1, end2, end3, end4]
+    for z,i in enumerate([1,3,5,7]): # count, item
+        for l in starts:
+            if math.isclose(finalstartstops[i], l, abs_tol = 1):
+                finalnumbers.append(l)
+                ends_list[z].append(finalnumbers[0])
+                finalnumbers = []
     return start1, end1, start2, end2, start3, end3, start4, end4
 
 def par_mod(input_par, output_par, start1, end1, start2, end2, start3, end3, start4, end4):
@@ -170,3 +123,4 @@ def main():
 if __name__ == "__main__":
     # execute only if run as a script
     main()
+
